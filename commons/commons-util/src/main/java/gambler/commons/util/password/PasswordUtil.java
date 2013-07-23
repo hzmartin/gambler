cb0e774e8d98100f293bd908be8271536bd8334c
Martin Wong <wqihui@yeah.net>
2013-07-23 11:28:54 +0800

    dos2unix

---
+++ commons/commons-util/src/main/java/gambler/commons/util/password/PasswordUtil.java
@@ -0,0 +1,23 @@
+package gambler.commons.util.password;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+/**
+ * PasswordUtil provides password util methods based on StrongPasswordEncryptor
+ * 
+ * @auther Martin
+ */
+
+public final class PasswordUtil {
+
+	private static final StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+
+	public static final String encrypt(String password) {
+		return passwordEncryptor.encryptPassword(password);
+	}
+
+	public static final boolean checkPassword(String encryptedPassword,
+			String clearTextPwd) {
+		return passwordEncryptor.checkPassword(clearTextPwd, encryptedPassword);
+	}
+}
