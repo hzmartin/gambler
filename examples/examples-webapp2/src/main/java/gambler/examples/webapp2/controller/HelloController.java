0cd047f4bbd75572515daacd4f00843b21216390
Martin Wong <hzwangqihui@gmail.com>
2013-10-11 10:07:01 +0800

    sample controller

--- examples/examples-webapp2/src/main/java/gambler/examples/webapp2/controller/HelloController.java
+++ examples/examples-webapp2/src/main/java/gambler/examples/webapp2/controller/HelloController.java
@@ -2,8 +2,11 @@ package gambler.examples.webapp2.controller;
 
 import gambler.commons.auth.User;
 import gambler.examples.webapp2.dao.UserDao;
+import gambler.examples.webapp2.dwr.DemoMessage;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -27,6 +31,9 @@ public class HelloController {
 		return new ModelAndView("welcome", "message", message);
 	}
 
+	/**
+	 * sample request: /demo/echo.do?message=xxx
+	 */
 	@RequestMapping(value = "/echo", method = { RequestMethod.POST,
 			RequestMethod.GET })
 	public ResponseEntity<String> echo(
@@ -34,10 +41,21 @@ public class HelloController {
 		return new ResponseEntity<String>("[echo]" + message, HttpStatus.OK);
 	}
 
-	@RequestMapping("/findUser")
-	public String findUserById(Model model, User user) {
+	/**
+	 * sample request: /demo/find.do?userId=xxx
+	 */
+	@RequestMapping("/find")
+	public String find(Model model, User user) {
 		User foundUser = userDao.findUserById(user.getUserId());
-		model.addAttribute("message", foundUser.getUserId() + ":" + foundUser.getFirstName());
+		model.addAttribute("message",
+				foundUser.getUserId() + ":" + foundUser.getFirstName());
 		return "welcome";
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	@ResponseBody
+	public Object search(final HttpServletRequest request,
+			final HttpServletResponse response) {
+		return new DemoMessage("demo json response");
 	}
 }
