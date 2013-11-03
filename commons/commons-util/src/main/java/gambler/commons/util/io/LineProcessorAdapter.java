33934bd2889e68577db194e34d9a4c38b1ad4bec
Martin Wong <hzwangqihui@gmail.com>
2013-11-03 12:55:32 +0800

    add lineprocessoradapter

---
+++ commons/commons-util/src/main/java/gambler/commons/util/io/LineProcessorAdapter.java
@@ -0,0 +1,12 @@
+package gambler.commons.util.io;
+
+/**
+ * @author Martin
+ */
+public abstract class LineProcessorAdapter implements ILineProcessor {
+
+    @Override
+    public void cleanUp() {
+    }
+
+}
