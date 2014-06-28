7f0e29b43d5607a7f22d66c01fba3c27086c0d32
Martin Wong <hzwangqihui@gmail.com>
2014-06-28 15:49:28 +0800

    add check perm feature

--- examples/examples-webapp2/src/main/java/gambler/examples/webapp2/dao/AuthDao.java
+++ examples/examples-webapp2/src/main/java/gambler/examples/webapp2/dao/AuthDao.java
@@ -1,13 +1,43 @@
 package gambler.examples.webapp2.dao;
 
-import gambler.examples.webapp2.domain.AuthUser;
+import gambler.examples.webapp2.domain.auth.Permission;
+import gambler.examples.webapp2.domain.auth.RolePermission;
+import gambler.examples.webapp2.domain.auth.User;
+import gambler.examples.webapp2.domain.auth.UserPermission;
+import gambler.examples.webapp2.domain.auth.UserRole;
+
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthUserDao {
+public interface AuthDao {
 
-	AuthUser findByUserId(String userId);
-	
-	int saveUser(AuthUser user);
+	User find(String userId);
+
+	int save(User user);
+
+	int update(User user);
+
+	int delete(String userId);
+
+	List<Permission> getAllPermissions();
+
+	List<UserRole> getUserRoles(long uid);
+
+	List<RolePermission> getAllRolePermissions();
+
+	List<UserPermission> getUserPermissions(long uid);
+
+	UserPermission getUserPermission(UserPermission up);
+
+	int createUserRole(UserRole ur);
+
+	int createUserPermission(UserPermission up);
+
+	int delUserPermission(UserPermission up);
+
+	int delUserPermissions(long uid);
+
+	int delUserRoles(long uid);
 }
