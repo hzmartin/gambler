2b7610a1e1024ffab6dfbf3442f4d0ad8f3e9e21
Martin Wong <hzwangqihui@gmail.com>
2014-07-24 16:40:09 +0800

    add freemarker

--- examples/examples-webapp2/src/main/java/gambler/examples/webapp2/controller/view/MiscViewController.java
+++ examples/examples-webapp2/src/main/java/gambler/examples/webapp2/controller/view/MiscViewController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -24,7 +25,7 @@ import org.springframework.web.servlet.ModelAndView;
  * @version 2013-10-18
  */
 @Controller
-public class MiscViewController extends AbstractController{
+public class MiscViewController extends AbstractController {
 
 	@RequestMapping(value = "/signin")
 	public Object signin(final HttpServletRequest request,
@@ -34,7 +35,7 @@ public class MiscViewController extends AbstractController{
 	}
 
 	@RequestMapping(value = "/index")
-	@AuthRequired(permission={AuthConstants.PERM_MISC.PERM_ENTER_HOME_PAGE})
+	@AuthRequired(permission = { AuthConstants.PERM_MISC.PERM_ENTER_HOME_PAGE })
 	public Object index(final HttpServletRequest request,
 			final HttpServletResponse response) {
 		ModelAndView result1 = new ModelAndView("index");
@@ -46,16 +47,14 @@ public class MiscViewController extends AbstractController{
 			for (int nidx = 1; nidx <= count; nidx++) {
 				String name = sysconf.getString("mainnav.name." + nidx);
 				String url = sysconf.getString("mainnav.url." + nidx);
-				if (StringUtils.isBlank(name)
-						|| StringUtils.isBlank(url)) {
+				if (StringUtils.isBlank(name) || StringUtils.isBlank(url)) {
 					continue;
 				}
 				Long pid = sysconf.getLong("mainnav.perm." + nidx);
 				if (pid != null) {
 					// check nav item perm
-					boolean hasThisPerm = authUserService
-							.checkUserPermission(loginUser.getUserId(),
-									pid);
+					boolean hasThisPerm = authUserService.checkUserPermission(
+							loginUser.getUserId(), pid);
 					if (hasThisPerm) {
 						// passed
 						menus.add(new NaviItemDto(name, url));
@@ -71,21 +70,24 @@ public class MiscViewController extends AbstractController{
 		return result1;
 	}
 
+	@RequestMapping(value = "/view")
+	public ModelAndView view(final HttpServletRequest request,
+			@RequestParam(required = false) String name) {
+		return new ModelAndView(name);
+	}
+
 	@RequestMapping(value = "/404")
-	public ModelAndView pagenotfound(final HttpServletRequest request,
-			final String name) {
+	public ModelAndView pagenotfound(final HttpServletRequest request) {
 		return new ModelAndView("404");
 	}
 
 	@RequestMapping(value = "/403")
-	public ModelAndView forbidden(final HttpServletRequest request,
-			final String name) {
+	public ModelAndView forbidden(final HttpServletRequest request) {
 		return new ModelAndView("403");
 	}
 
 	@RequestMapping(value = "/500")
-	public ModelAndView serverbusy(final HttpServletRequest request,
-			final String name) {
+	public ModelAndView serverbusy(final HttpServletRequest request) {
 		return new ModelAndView("500");
 	}
 
