f7673c919b31d2bdc846f8557d1ac22f64fbebf2
Martin Wong <hzwangqihui@gmail.com>
2014-07-29 09:49:10 +0800

    update job listener

--- examples/examples-webapp2/src/main/java/gambler/examples/webapp2/listener/SimpleJobListener.java
+++ examples/examples-webapp2/src/main/java/gambler/examples/webapp2/listener/SimpleJobListener.java
@@ -19,19 +19,25 @@ public class SimpleJobListener implements JobListener {
 	}
 
 	public void jobToBeExecuted(JobExecutionContext context) {
-		String jobName = context.getJobDetail().getName();
+		String jobName = context.getJobDetail().getFullName();
 		logger.info(jobName + " is about to be executed");
 	}
 
 	public void jobExecutionVetoed(JobExecutionContext context) {
-		String jobName = context.getJobDetail().getName();
+		String jobName = context.getJobDetail().getFullName();
 		logger.info(jobName + " was vetoed and not executed()");
 	}
 
 	public void jobWasExecuted(JobExecutionContext context,
 			JobExecutionException jobException) {
 
-		String jobName = context.getJobDetail().getName();
-		logger.info(jobName + " was executed");
+		String jobName = context.getJobDetail().getFullName();
+		if (jobException == null) {
+			logger.info(jobName
+					+ " was executed successfully without exception");
+		} else {
+			logger.error(jobName + " was executed with exception("
+					+ jobException.getMessage() + ")!");
+		}
 	}
 }
