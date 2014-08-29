945d2cc2807730f014b244d699b9b3a37885db7e
Martin Wong <hzwangqihui@gmail.com>
2014-08-29 19:50:20 +0800

    add debug switch

--- tools/cli/src/main/java/gambler/tools/cli/CLI.java
+++ tools/cli/src/main/java/gambler/tools/cli/CLI.java
@@ -11,105 +11,112 @@ import java.util.Scanner;
 
 /**
  * Command Line Tools Main Class
- *
+ * 
  * @author Martin
- *
+ * 
  */
 public class CLI {
 
-    /**
-     * @param args
-     * @throws CommandNameConflictException
-     */
-    public static void main(String[] args) throws CommandNameConflictException {
-        // welcome and display system menu
-        System.out.println();
-        System.out
-                .println("=========================================================");
-        System.out.println("||");
-        System.out.println("|| Welcome to use Gambler CLI :)");
-        System.out.println("||");
-        System.out.println("|| Author: Martin hzwangqihui@gmail.com");
-        System.out
-                .println("|| Any problem please don't hesitate to contact me!");
-        System.out.println("|| Note: try command 'help' first");
-        System.out
-                .println("=========================================================");
+	/**
+	 * @param args
+	 * @throws CommandNameConflictException
+	 */
+	public static void main(String[] args) throws CommandNameConflictException {
+		// welcome and display system menu
+		System.out.println();
+		System.out
+				.println("=========================================================");
+		System.out.println("||");
+		System.out.println("|| Welcome to use Gambler CLI :)");
+		System.out.println("||");
+		System.out.println("|| Author: Martin hzwangqihui@gmail.com");
+		System.out
+				.println("|| Any problem please don't hesitate to contact me!");
+		System.out.println("|| Note: try command 'help' first");
+		System.out
+				.println("=========================================================");
 
-        // init system
-        CLISystem cli = new CLISystem();
-        cli.init();
-        Scanner console = new Scanner(System.in);
-        while (true) {
+		// init system
+		CLISystem cli = new CLISystem();
+		cli.init();
+		Scanner console = new Scanner(System.in);
+		while (true) {
 
-            System.out.print("CLI>> ");
-            String cmdString = console.nextLine();
-            if (cmdString == null || cmdString.trim().isEmpty()) {
-                continue;
-            }
-            ICommand cmd = null;
-            try {
-                String[] params = parseCmdString(cmdString.trim());
-                cmd = cli.createCommand(params);
-                if (cmd == null) {
-                    System.out.println("Unknown command, try with 'help'");
-                    continue;
-                }
+			System.out.print("CLI>> ");
+			String cmdString = console.nextLine();
+			if (cmdString == null || cmdString.trim().isEmpty()) {
+				continue;
+			}
+			ICommand cmd = null;
+			try {
+				String[] params = parseCmdString(cmdString.trim());
+				cmd = cli.createCommand(params);
+				if (cmd == null) {
+					System.out.println("Unknown command, try with 'help'");
+					continue;
+				}
 
-                if (!cmd.isIgnorableCommand()) {
-                    cli.setPrevCommand(cmd);
-                    cli.addHistoryCommand(cmd);
-                }
-                cmd.setCLISystem(cli);
+				if (!cmd.isIgnorableCommand()) {
+					cli.setPrevCommand(cmd);
+					cli.addHistoryCommand(cmd);
+				}
+				cmd.setCLISystem(cli);
 
-                cmd.execute();
-            } catch (CommandExecException ex) {
-                System.out.println("execute comand [" + cmd + "] error!");
-            } catch (CommandUsageException ex) {
-                cmd.printCommandUsage();
-            } catch (Exception e) {
-                e.printStackTrace(System.out);
-            }
+				cmd.execute();
+			} catch (CommandExecException ex) {
+				System.out.println("execute comand [" + cmd + "] error!");
+				if (isDebugOn()) {
+					ex.printStackTrace(System.err);
+				}
+			} catch (CommandUsageException ex) {
+				cmd.printCommandUsage();
+			}
 
-        }
+		}
 
-    }
+	}
 
-    /**
-     * @param cmdString
-     * @return
-     */
-    private static String[] parseCmdString(String line) throws CommandUsageException {
-        String[] columnArray = null;
+	public static boolean isDebugOn() {
+		return CLISystem.SYSCONFIG.getBoolean("cli.debug", false);
+	}
 
-        // 参数带引号的情况（暂只支持单引号）
-        if (line.contains("'")) {
-            String[] tmpArray = line.split("'");
+	/**
+	 * @param cmdString
+	 * @return
+	 */
+	private static String[] parseCmdString(String line)
+			throws CommandUsageException {
+		String[] columnArray = null;
 
-            // 引号数量不匹配
-            if (tmpArray.length % 2 == 0 && !line.endsWith("'")) {
-                throw new CommandUsageException("single quotation marks is not matched!");
-            }
+		// 参数带引号的情况（暂只支持单引号）
+		if (line.contains("'")) {
+			String[] tmpArray = line.split("'");
 
-            List<String> columnList = new ArrayList<String>();
-            for (int i = 0; i < tmpArray.length; i++) {
-                String tmp = tmpArray[i].trim();
-                // 一对引号之间的部分即单个参数
-                if (i % 2 != 0) {
-                    columnList.add(tmp);
-                } // 引号以外的部分可能含多个参数，需要再分割
-                else {
-                    String[] tmpArray2 = tmp.split("\\s+");
-                    columnList.addAll(Arrays.asList(tmpArray2));
-                }
-            }
+			// 引号数量不匹配
+			if (tmpArray.length % 2 == 0 && !line.endsWith("'")) {
+				throw new CommandUsageException(
+						"single quotation marks is not matched!");
+			}
 
-            columnArray = columnList.toArray(new String[columnList.size()]);
-        } else {
-            columnArray = line.split("\\s+");
-        }
+			List<String> columnList = new ArrayList<String>();
+			for (int i = 0; i < tmpArray.length; i++) {
+				String tmp = tmpArray[i].trim();
+				// 一对引号之间的部分即单个参数
+				if (i % 2 != 0) {
+					columnList.add(tmp);
+				} // 引号以外的部分可能含多个参数，需要再分割
+				else {
+					String[] tmpArray2 = tmp.split("\\s+");
+					columnList.addAll(Arrays.asList(tmpArray2));
+				}
+			}
 
-        return columnArray;
-    }
+			columnArray = columnList.toArray(new String[columnList.size()]);
+		} else {
+			columnArray = line.split("\\s+");
+		}
+
+		return columnArray;
+	}
 
 }
