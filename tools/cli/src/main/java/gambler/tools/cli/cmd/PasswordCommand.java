2647119a05f31b27a752b87f1cf2269255f62606
hzwangqh <hzwangqihui@gmail.com>
2017-12-31 19:35:16 +0800

    update dtd.

--- tools/cli/src/main/java/gambler/tools/cli/cmd/PasswordCommand.java
+++ tools/cli/src/main/java/gambler/tools/cli/cmd/PasswordCommand.java
@@ -2,48 +2,63 @@ package gambler.tools.cli.cmd;
 
 import gambler.tools.service.PasswordService;
 import gambler.tools.service.ServiceException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PasswordCommand extends AbstractCommand implements ICommand {
 
-	private PasswordService pService = new PasswordService();
+    private PasswordService pService = new PasswordService();
 
-	public PasswordCommand() {
-		super("password");
-	}
+    public PasswordCommand() {
+        super("password");
+    }
 
-	@Override
-	public void service(String[] params) throws CommandUsageException,
-			ServiceException {
-		if (isSubCommand("encrypt")) {
-			String password = params[1];
-			String encrypt = pService.encrypt(password);
-			System.out.println(String.format("encrypted password for %s: %s",
-					password, encrypt));
-		} else if (isSubCommand("decrypt")) {
-			String password = params[1];
-			String decrypt = pService.decrypt(password);
-			System.out
-					.println(String.format("decrypted password: %s", decrypt));
-		} else {
-			throw new CommandUsageException("command usage error!");
-		}
+    @Override
+    public void service(String[] params) throws CommandUsageException,
+            ServiceException {
+        if (isSubCommand("encrypt")) {
+            String password = params[1];
+            String encrypt = pService.encrypt(password);
+            System.out.println(String.format("encrypted password for %s: %s",
+                    password, encrypt));
+        } else if (isSubCommand("decrypt")) {
+            String password = params[1];
+            String decrypt = pService.decrypt(password);
+            System.out
+                    .println(String.format("decrypted password: %s", decrypt));
+        } else if (isSubCommand("save")) {
+            try {
+                String uid = params[1];
+                String site = params[2];
+                String type = params[3];
+                String password = params[4];
+                pService.save(uid, site, type, password);
+                System.out
+                        .println(String.format("saved successfully: %s, %s", uid, site));
+            } catch (IOException ex) {
+                throw new ServiceException(ex);
+            }
+        } else {
+            throw new CommandUsageException("command usage error!");
+        }
 
-	}
+    }
 
-	@Override
-	public String[] getDescription() {
-		return new String[] { "encrypt password", "decrypt password" };
-	}
+    @Override
+    public String[] getDescription() {
+        return new String[]{"encrypt password", "decrypt password"};
+    }
 
-	@Override
-	public String[] getSyntax() {
-		return new String[] { "password encrypt /password/",
-				"password decrypt /password/" };
-	}
+    @Override
+    public String[] getSyntax() {
+        return new String[]{"password encrypt /password/",
+            "password decrypt /password/"};
+    }
 
-	@Override
-	public String[] getAlias() {
-		return new String[] { "pass", "pa" };
-	}
+    @Override
+    public String[] getAlias() {
+        return new String[]{"pass", "pa"};
+    }
 
 }
--- tools/cli/src/main/java/gambler/tools/service/PasswordService.java
+++ tools/cli/src/main/java/gambler/tools/service/PasswordService.java
@@ -3,41 +3,55 @@ package gambler.tools.service;
 import gambler.tools.cli.CLISystem;
 import gambler.commons.advmap.XMLMap;
 import gambler.commons.util.jasypt.GBEnvironmentStringPBEConfig;
+import gambler.tools.cli.bean.EncryptedPassword;
+import gambler.tools.cli.util.DBUtil;
+import java.io.IOException;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.ibatis.session.SqlSession;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 public class PasswordService {
 
-	private static final XMLMap sysConfig = CLISystem.SYSCONFIG;
+    private static final XMLMap sysConfig = CLISystem.SYSCONFIG;
 
-	public String encrypt(String password) {
-		return getEncryptor().encrypt(password);
-	}
+    public String encrypt(String password) {
+        return getEncryptor().encrypt(password);
+    }
 
-	private StandardPBEStringEncryptor getEncryptor() {
-		String ePassword = sysConfig.getString("password.password");
-		if (StringUtils.isEmpty(ePassword)) {
-			System.out
-					.println("WARN: password missing, default encrypt password will be used!");
-		}
-		String algorithm = sysConfig.getString("password.algorithm");
-		if (StringUtils.isEmpty(algorithm)) {
-			System.out
-					.println("WARN: algorightm missing, PBEWithMD5AndDES will be used");
-			algorithm = "PBEWithMD5AndDES";
-		}
-		StandardPBEStringEncryptor encryptor = new org.jasypt.encryption.pbe.StandardPBEStringEncryptor();
-		GBEnvironmentStringPBEConfig config = new GBEnvironmentStringPBEConfig();
-		config.setAlgorithm(algorithm);
-		config.setPassword(ePassword);
-		encryptor.setConfig(config);
-		return encryptor;
-	}
+    private StandardPBEStringEncryptor getEncryptor() {
+        String ePassword = sysConfig.getString("password.password");
+        if (StringUtils.isEmpty(ePassword)) {
+            System.out
+                    .println("WARN: password missing, default encrypt password will be used!");
+        }
+        String algorithm = sysConfig.getString("password.algorithm");
+        if (StringUtils.isEmpty(algorithm)) {
+            System.out
+                    .println("WARN: algorightm missing, PBEWithMD5AndDES will be used");
+            algorithm = "PBEWithMD5AndDES";
+        }
+        StandardPBEStringEncryptor encryptor = new org.jasypt.encryption.pbe.StandardPBEStringEncryptor();
+        GBEnvironmentStringPBEConfig config = new GBEnvironmentStringPBEConfig();
+        config.setAlgorithm(algorithm);
+        config.setPassword(ePassword);
+        encryptor.setConfig(config);
+        return encryptor;
+    }
 
-	public String decrypt(String password) {
-		return getEncryptor().decrypt(password);
+    public String decrypt(String password) {
+        return getEncryptor().decrypt(password);
 
-	}
+    }
+
+    public void save(String uid, String site, String type, String password) throws IOException {
+        EncryptedPassword p = new EncryptedPassword();
+        p.setUid(uid);
+        p.setType(type);
+        p.setSite(site);
+        p.setPasswd(password);
+        SqlSession session = DBUtil.getSqlSessionFactory().openSession(false);
+        session.insert("EncryptedPassword.insert", p);
+    }
 
 }
