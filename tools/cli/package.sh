3c81d2abc50be065f0ec523b8d8a816f640501ef
hzwangqh <hzwangqihui@gmail.com>
2017-12-31 22:28:24 +0800

    update package.sh

--- tools/cli/package.sh
+++ tools/cli/package.sh
@@ -6,7 +6,9 @@ mkdir $rel_path
 mvn clean package -DskipTests
 
 cp scripts/go.sh $rel_path
+chmod +x $rel_path/go.sh
 cp target/classes/*.xml $rel_path
 cp target/classes/datasource.properties $rel_path
 cp target/cli.jar $rel_path
 cp -R target/lib $rel_path
+
