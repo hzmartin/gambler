111d7be897bd337f55025a1619c9b52f1dbbfe7f
hzwangqh <hzwangqihui@gmail.com>
2018-01-07 00:04:27 +0800

    gann update

--- tools/cli/src/main/java/gambler/tools/cli/cmd/GannCommand.java
+++ tools/cli/src/main/java/gambler/tools/cli/cmd/GannCommand.java
@@ -9,18 +9,18 @@ import gambler.tools.service.ServiceException;
 public class GannCommand extends AbstractCommand implements ICommand {
 
 	private GannService gann = new GannService();
-	
+
 	public GannCommand() {
 		super("gann");
 	}
 
 	@Override
 	public void service(String[] params) throws CommandUsageException, ServiceException {
-		if (isSubCommand("price4")) {
+		if (isSubCommand("p4")) {
 			double startprice = Double.valueOf(params[1]);// 起始价格或者指数
 			double pricestep = Double.valueOf(params[2]);// 价格或指数步长，比如每格升100点
 			gann.printGann4Price13(startprice, pricestep);
-		}if (isSubCommand("time4")) {
+		} else if (isSubCommand("t4")) {
 			String starttime = params[1];// 起始时间
 			String timeunit = params[2];// 时间单位，可选值：d/w/m，分别代表日d/周w/月m
 			gann.printGann4Time13(starttime, timeunit);
@@ -37,12 +37,12 @@ public class GannCommand extends AbstractCommand implements ICommand {
 
 	@Override
 	public String[] getSyntax() {
-		return new String[] { "gann price4 /startprice/ /pricestep/", "gann time4 /starttime/ timeunit/" };
+		return new String[] { "gann p4 /startprice/ /pricestep/", "gann t4 /starttime/ timeunit/" };
 	}
 
 	@Override
 	public String[] getAlias() {
-		return new String[] {"gann"};
+		return new String[] { "ga" };
 	}
 
 }
